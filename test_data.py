90654e22777c2f44eb04bbb63c55ae94c535262f
Vitalii <77769355+Vitaly-72@users.noreply.github.com>
2025-09-04 14:54:07 +0500

    Create test_data.py

---
+++ test_data.py
@@ -0,0 +1,44 @@
+# test_data.py
+TRIMET_TEST_HTML = """
+<div class="product-list">
+    <div class="product-list__item-roznica">
+        <div class="product-list__info">
+            <p class="product-list__name">
+                <a class="data_name_product" href="/catalog/armatura_6_3_metra/">
+                    Арматура 6 (3 метра)
+                </a>
+            </p>
+        </div>
+        <div class="product-list__price">
+            <p class="price-type-roznica">50 руб./шт</p>
+        </div>
+    </div>
+    <div class="product-list__item-roznica">
+        <div class="product-list__info">
+            <p class="product-list__name">
+                <a class="data_name_product" href="/catalog/armatura_6_6_metrov/">
+                    Арматура 6 (6 метров)
+                </a>
+            </p>
+        </div>
+        <div class="product-list__price">
+            <p class="price-type-roznica">90 руб./шт</p>
+        </div>
+    </div>
+</div>
+"""
+
+PARAD_TEST_HTML = """
+<div class="product-thumb">
+    <div class="caption">
+        <h4><a href="#">Арматура 6 мм AIII 6м</a></h4>
+        <p class="price"><span class="price-new">88р.</span></p>
+    </div>
+</div>
+<div class="product-thumb">
+    <div class="caption">
+        <h4><a href="#">Арматура 8 мм AIII 6м</a></h4>
+        <p class="price"><span class="price-new">150р.</span></p>
+    </div>
+</div>
+"""
