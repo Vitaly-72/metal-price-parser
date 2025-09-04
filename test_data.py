0849b91304ea47fa0dcd76171d59c7f5ba5fe6e3
Vitalii <77769355+Vitaly-72@users.noreply.github.com>
2025-09-04 14:56:40 +0500

    Update test_data.py

--- test_data.py
+++ test_data.py
@@ -4,9 +4,7 @@ TRIMET_TEST_HTML = """
     <div class="product-list__item-roznica">
         <div class="product-list__info">
             <p class="product-list__name">
-                <a class="data_name_product" href="/catalog/armatura_6_3_metra/">
-                    Арматура 6 (3 метра)
-                </a>
+                <a class="data_name_product">Арматура 6 (3 метра)</a>
             </p>
         </div>
         <div class="product-list__price">
@@ -16,13 +14,31 @@ TRIMET_TEST_HTML = """
     <div class="product-list__item-roznica">
         <div class="product-list__info">
             <p class="product-list__name">
-                <a class="data_name_product" href="/catalog/armatura_6_6_metrov/">
-                    Арматура 6 (6 метров)
-                </a>
+                <a class="data_name_product">Арматура 6 (6 метров)</a>
             </p>
         </div>
         <div class="product-list__price">
             <p class="price-type-roznica">90 руб./шт</p>
+        </div>
+    </div>
+    <div class="product-list__item-roznica">
+        <div class="product-list__info">
+            <p class="product-list__name">
+                <a class="data_name_product">Арматура 8 (6 метров)</a>
+            </p>
+        </div>
+        <div class="product-list__price">
+            <p class="price-type-roznica">160 руб./шт</p>
+        </div>
+    </div>
+    <div class="product-list__item-roznica">
+        <div class="product-list__info">
+            <p class="product-list__name">
+                <a class="data_name_product">Арматура 10 (5,85 метров)</a>
+            </p>
+        </div>
+        <div class="product-list__price">
+            <p class="price-type-roznica">230 руб./шт</p>
         </div>
     </div>
 </div>
@@ -31,14 +47,26 @@ TRIMET_TEST_HTML = """
 PARAD_TEST_HTML = """
 <div class="product-thumb">
     <div class="caption">
-        <h4><a href="#">Арматура 6 мм AIII 6м</a></h4>
+        <h4><a>Арматура 6 мм AIII 6м</a></h4>
         <p class="price"><span class="price-new">88р.</span></p>
     </div>
 </div>
 <div class="product-thumb">
     <div class="caption">
-        <h4><a href="#">Арматура 8 мм AIII 6м</a></h4>
+        <h4><a>Арматура 8 мм AIII 6м</a></h4>
         <p class="price"><span class="price-new">150р.</span></p>
+    </div>
+</div>
+<div class="product-thumb">
+    <div class="caption">
+        <h4><a>Арматура 10 мм AIII 5,85м</a></h4>
+        <p class="price"><span class="price-new">225р.</span></p>
+    </div>
+</div>
+<div class="product-thumb">
+    <div class="caption">
+        <h4><a>Арматура 12 мм AIII 5,85м</a></h4>
+        <p class="price"><span class="price-new">288р.</span></p>
     </div>
 </div>
 """
