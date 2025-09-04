56eb19a2472a513aca83aced19ebf845d06b4b2d
Vitalii <77769355+Vitaly-72@users.noreply.github.com>
2025-09-04 15:07:40 +0500

    Update test_data.py

--- test_data.py
+++ test_data.py
@@ -2,15 +2,39 @@
 TRIMET_TEST_HTML = """<div class="product-list">
 <div class="product-list__item-roznica">
 <div class="product-list__info">
+<p class="product-list__name"><a class="data_name_product">Арматура 6 (3 метра)</a></p>
+</div>
+<div class="product-list__price"><p class="price-type-roznica">50 руб./шт</p></div>
+</div>
+<div class="product-list__item-roznica">
+<div class="product-list__info">
 <p class="product-list__name"><a class="data_name_product">Арматура 6 (6 метров)</a></p>
 </div>
 <div class="product-list__price"><p class="price-type-roznica">90 руб./шт</p></div>
 </div>
 <div class="product-list__item-roznica">
 <div class="product-list__info">
+<p class="product-list__name"><a class="data_name_product">Арматура 8 (3 метра)</a></p>
+</div>
+<div class="product-list__price"><p class="price-type-roznica">80 руб./шт</p></div>
+</div>
+<div class="product-list__item-roznica">
+<div class="product-list__info">
 <p class="product-list__name"><a class="data_name_product">Арматура 8 (6 метров)</a></p>
 </div>
 <div class="product-list__price"><p class="price-type-roznica">160 руб./шт</p></div>
+</div>
+<div class="product-list__item-roznica">
+<div class="product-list__info">
+<p class="product-list__name"><a class="data_name_product">Арматура 10 (5,85 метров)</a></p>
+</div>
+<div class="product-list__price"><p class="price-type-roznica">230 руб./шт</p></div>
+</div>
+<div class="product-list__item-roznica">
+<div class="product-list__info">
+<p class="product-list__name"><a class="data_name_product">Арматура 12 (5,85 метров)</a></p>
+</div>
+<div class="product-list__price"><p class="price-type-roznica">310 руб./шт</p></div>
 </div>
 </div>"""
 
@@ -24,5 +48,23 @@ PARAD_TEST_HTML = """<div class="product-thumb">
 <div class="caption">
 <h4><a>Арматура 8 мм AIII 6м</a></h4>
 <p class="price"><span class="price-new">150р.</span></p>
+</div>
+</div>
+<div class="product-thumb">
+<div class="caption">
+<h4><a>Арматура 10 мм AIII 5,85м</a></h4>
+<p class="price"><span class="price-new">225р.</span></p>
+</div>
+</div>
+<div class="product-thumb">
+<div class="caption">
+<h4><a>Арматура 12 мм AIII 5,85м</a></h4>
+<p class="price"><span class="price-new">288р.</span></p>
+</div>
+</div>
+<div class="product-thumb">
+<div class="caption">
+<h4><a>Арматура 14 мм AIII 5,85м</a></h4>
+<p class="price"><span class="price-new">400р.</span></p>
 </div>
 </div>"""
