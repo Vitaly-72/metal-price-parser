9d4f3ef3b1c47b3f0e218ba49c65f5006f0ff6db
Vitalii <77769355+Vitaly-72@users.noreply.github.com>
2025-09-04 16:02:56 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,25 +2,20 @@
 
 # Тримет - арматура (несколько страниц)
 TRIMET_ARMATURA_URLS = [
-    "https://trimet.ru/catalog/chernyy_metalloprokat/armatura_1/",
-    "https://trimet.ru/catalog/chernyy_metalloprokat/armatura_1/?PAGEN_1=2",
-    "https://trimet.ru/catalog/chernyy_metalloprokat/armatura_1/?PAGEN_1=3"
+    "https://trimet.ru/catalog/chernyy_metalloprokat/armatura_1/"
 ]
 
 # Парад - арматура (несколько страниц)  
 PARAD_ARMATURA_URLS = [
-    "https://72parad.ru/metalloprokat/armatura/",
-    "https://72parad.ru/metalloprokat/armatura/?page=2",
-    "https://72parad.ru/metalloprokat/armatura/?page=3"
+    "https://72parad.ru/metalloprokat/armatura/"
 ]
 
 # Другие категории (пример)
 TRIMET_TRUBA_URLS = [
-    "https://trimet.ru/catalog/chernyy_metalloprokat/truba_profilnaya/",
-    "https://trimet.ru/catalog/chernyy_metalloprokat/truba_profilnaya/?PAGEN_1=2"
+    "https://trimet.ru/catalog/chernyy_metalloprokat/truby_profilnye/?PAGEN_1=1",
+    "https://trimet.ru/catalog/chernyy_metalloprokat/truby_profilnye/?PAGEN_1=2"
 ]
 
 PARAD_TRUBA_URLS = [
-    "https://72parad.ru/metalloprokat/truba-profilnaya/",
-    "https://72parad.ru/metalloprokat/truba-profilnaya/?page=2"
+    "https://72parad.ru/metalloprokat/truba-profilnaya/"
 ]
