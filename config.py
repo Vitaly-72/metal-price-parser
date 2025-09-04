780734b53f74f1cb83a4b57faa634411d4b8949d
Vitalii <77769355+Vitaly-72@users.noreply.github.com>
2025-09-04 16:24:45 +0500

    Update config.py

--- config.py
+++ config.py
@@ -19,3 +19,12 @@ TRIMET_TRUBA_URLS = [
 PARAD_TRUBA_URLS = [
     "https://72parad.ru/metalloprokat/truba-profilnaya/"
 ]
+# Тестовые данные для Тримет (на основе вашего HTML)
+TRIMET_TEST_DATA = [
+    {"name": "Арматура 6 (3 метра)", "price": 50, "site": "Тримет"},
+    {"name": "Арматура 6 (6 метров)", "price": 90, "site": "Тримет"},
+    {"name": "Арматура 8 (3 метра)", "price": 80, "site": "Тримет"},
+    {"name": "Арматура 8 (6 метров)", "price": 160, "site": "Тримет"},
+    {"name": "Арматура 10 (5,85 метров)", "price": 230, "site": "Тримет"},
+    {"name": "Арматура 12 (5,85 метров)", "price": 310, "site": "Тримет"}
+]
