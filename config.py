9f90f9263fe310504e85f0574bbf9738972f116a
Vitalii <77769355+Vitaly-72@users.noreply.github.com>
2025-09-04 14:42:19 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,15 +4,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    # URLs для парсинга
+    # URLs для парсинга разных категорий
     TRIMET_URLS = {
         'armatura': os.getenv('TRIMET_ARMATURA_URL', ''),
-        'truba_profilnaya': os.getenv('TRIMET_TRUBA_URL', '')
+        'truba_profilnaya': os.getenv('TRIMET_TRUBA_PROFILNAYA_URL', ''),
+        'ugolok': os.getenv('TRIMET_UGOLOK_URL', ''),
+        'shveller': os.getenv('TRIMET_SHVELLER_URL', '')
     }
     
     PARAD_URLS = {
         'armatura': os.getenv('PARAD_ARMATURA_URL', ''),
-        'truba_profilnaya': os.getenv('PARAD_TRUBA_URL', '')
+        'truba_profilnaya': os.getenv('PARAD_TRUBA_PROFILNAYA_URL', ''),
+        'ugolok': os.getenv('PARAD_UGOLOK_URL', ''),
+        'shveller': os.getenv('PARAD_SHVELLER_URL', '')
     }
     
     # Настройки парсера
