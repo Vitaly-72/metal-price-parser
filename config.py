adc0f53b7fda32a6ec87e48512771041615e94e4
Vitalii <77769355+Vitaly-72@users.noreply.github.com>
2025-09-04 15:56:34 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,26 @@
-import os
-from dotenv import load_dotenv
+# config.py
 
-load_dotenv()
+# Тримет - арматура (несколько страниц)
+TRIMET_ARMATURA_URLS = [
+    "https://trimet.ru/catalog/chernyy_metalloprokat/armatura_1/",
+    "https://trimet.ru/catalog/chernyy_metalloprokat/armatura_1/?PAGEN_1=2",
+    "https://trimet.ru/catalog/chernyy_metalloprokat/armatura_1/?PAGEN_1=3"
+]
 
-class Config:
-    # URLs для парсинга разных категорий
-    TRIMET_URLS = {
-        'armatura': os.getenv('TRIMET_ARMATURA_URL', ''),
-        'truba_profilnaya': os.getenv('TRIMET_TRUBA_PROFILNAYA_URL', ''),
-        'ugolok': os.getenv('TRIMET_UGOLOK_URL', ''),
-        'shveller': os.getenv('TRIMET_SHVELLER_URL', '')
-    }
-    
-    PARAD_URLS = {
-        'armatura': os.getenv('PARAD_ARMATURA_URL', ''),
-        'truba_profilnaya': os.getenv('PARAD_TRUBA_PROFILNAYA_URL', ''),
-        'ugolok': os.getenv('PARAD_UGOLOK_URL', ''),
-        'shveller': os.getenv('PARAD_SHVELLER_URL', '')
-    }
-    
-    # Настройки парсера
-    REQUEST_TIMEOUT = 30
-    MAX_RETRIES = 3
+# Парад - арматура (несколько страниц)  
+PARAD_ARMATURA_URLS = [
+    "https://72parad.ru/metalloprokat/armatura/",
+    "https://72parad.ru/metalloprokat/armatura/?page=2",
+    "https://72parad.ru/metalloprokat/armatura/?page=3"
+]
+
+# Другие категории (пример)
+TRIMET_TRUBA_URLS = [
+    "https://trimet.ru/catalog/chernyy_metalloprokat/truba_profilnaya/",
+    "https://trimet.ru/catalog/chernyy_metalloprokat/truba_profilnaya/?PAGEN_1=2"
+]
+
+PARAD_TRUBA_URLS = [
+    "https://72parad.ru/metalloprokat/truba-profilnaya/",
+    "https://72parad.ru/metalloprokat/truba-profilnaya/?page=2"
+]
